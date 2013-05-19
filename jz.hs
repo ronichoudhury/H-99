528a840f1337c74a8434cf08e6d18db956ae9022
Roni Choudhury <roni@cs.utah.edu>
2013-05-18 23:44:06 -0600

    Solved problem 2, improved solution for problem 1.

--- jz.hs
+++ jz.hs
@@ -1,4 +1,9 @@
 myLast :: [a] -> a
-myLast [] = error "myLast called on empty list"
 myLast (x:[]) = x
 myLast (x:xs) = myLast xs
+myLast _ = error "myLast called on empty list"
+
+myButLast :: [a] -> a
+myButLast (x:y:[]) = x
+myButLast (x:xs) = myButLast xs
+myButLast _ = error "myButLast called on too short a list"
