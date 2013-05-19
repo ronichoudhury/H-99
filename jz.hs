c155e0dfdb6023122466c68bd335f801874d7b44
Roni Choudhury <roni@cs.utah.edu>
2013-05-18 23:52:03 -0600

    Solved problem 4.

--- jz.hs
+++ jz.hs
@@ -12,3 +12,7 @@ elementAt :: [a] -> Int -> a
 elementAt (x:_) 1 = x
 elementAt (_:xs) k = elementAt xs $ k - 1
 elementAt _ _ = error "Index out of range"
+
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (_:xs) = 1 + myLength xs
