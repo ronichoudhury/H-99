1e4ebf6ddfd2e2b415f766adb2c802a0007877c1
Roni Choudhury <roni@cs.utah.edu>
2013-05-18 23:49:08 -0600

    Solved problem 3.

--- jz.hs
+++ jz.hs
@@ -7,3 +7,8 @@ myButLast :: [a] -> a
 myButLast (x:y:[]) = x
 myButLast (x:xs) = myButLast xs
 myButLast _ = error "myButLast called on too short a list"
+
+elementAt :: [a] -> Int -> a
+elementAt (x:_) 1 = x
+elementAt (_:xs) k = elementAt xs $ k - 1
+elementAt _ _ = error "Index out of range"
