724439b9e83d7938a6057eabf02b575e9361a569
Roni Choudhury <roni@cs.utah.edu>
2013-05-19 18:31:11 -0600

    Solved problem 13 (also, broke rle function out of earlier solution).

--- jz.hs
+++ jz.hs
@@ -65,13 +65,14 @@ encode xs = map (\x -> (myLength x, head x)) $ pack xs
 
 -- Problem 11
 data RLE a = Multiple Int a | Single a deriving (Show)
+rle :: (Eq a) => (Int, a) -> RLE a
+rle (n, x)
+    | n == 1 = Single x
+    | n > 1 = Multiple n x
+    | otherwise = error "Impossible RLE spec"
+
 encodeModified :: (Eq a) => [a] -> [RLE a]
 encodeModified = map rle . encode
-    where rle :: (Eq a) => (Int, a) -> RLE a
-          rle (n, x)
-              | n == 1 = Single x
-              | n > 1 = Multiple n x
-              | otherwise = error "Impossible RLE spec"
 
 -- Problem 12
 decodeModified :: [RLE a] -> [a]
@@ -79,6 +80,20 @@ decodeModified = concatMap decodeRLE
     where decodeRLE :: RLE a -> [a]
           decodeRLE (Single a) = [a]
           decodeRLE (Multiple n a) = take n (repeat a)
+
+-- Problem 13
+encodeDirect :: (Eq a) => [a] -> [RLE a]
+encodeDirect [] = []
+encodeDirect (x:[]) = [Single x]
+encodeDirect xs =
+    let count :: (Eq a) => [a] -> (Int, a, [a])
+        count [x] = (1, x, [])
+        count (x:y:xs)
+            | x == y = (1 + c, x, r)
+            | otherwise = (1, x, y:xs)
+            where (c, _, r) = count $ y:xs 
+        (c, ch, r) = count xs
+    in rle (c, ch):(encodeDirect r)
 
 -- Problem 14
 dupli :: [a] -> [a]
